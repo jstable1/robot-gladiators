54c875f77cda586990fdd20d650f326f62af5220
jstable1 <jessica.stabler@gmail.com>
2022-01-14 21:29:49 -0700

    created loop for battling all combatants until defeated

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,8 +21,8 @@ for(var i = 0; i < enemyNames.length; i++) {
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  // repeat and execute as long as the enemy-robot is alive
+  while(enemyHealth > 0) {
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -73,9 +73,13 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
-};
+}
+}
+
 
 // run fight function to start game
-for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
